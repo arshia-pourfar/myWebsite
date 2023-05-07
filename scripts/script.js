31329778f31493a8d4eaa4a013653d1b6bd57ac4
arshia-pourfar <87662074+arshia-pourfar@users.noreply.github.com>
2023-05-07 18:53:49 +0330

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+
+  if ($(window).width() >= 330) {
+    $(".myName").animate({ fontSize: '200%' }, 1000);
+    $(".myName span").animate({ fontSize: '110%' }, 1000);
+  } else {
+    $(".myName").animate({ fontSize: '160%' }, 1000);
+    $(".myName span").animate({ fontSize: '110%' }, 1000);
+  }
   $('.multiscroll').multiscroll({
     verticalCentered: true,
     scrollingSpeed: 900,
@@ -34,7 +42,15 @@ $(document).ready(function () {
 
     //events
     onLeave: function (index, nextIndex, direction) { },
-    afterLoad: function (anchorLink, index) { },
+    afterLoad: function (anchorLink, index) {
+      if (index == 1) {
+        $('#navbar').removeClass('navbar-light');
+        $('#navbar').addClass('navbar-dark');
+      } else {
+        $('#navbar').removeClass('navbar-dark');
+        $('#navbar').addClass('navbar-light');
+      }
+    },
     afterRender: function () { },
     afterResize: function () { },
   });
@@ -56,10 +72,34 @@ $(document).ready(function () {
   // input text for typing animation
   $("#holder").writeText("Web And Wordpress Developer ");
 
-  // Fade Transition
-  var splide8 = new Splide('.splide-8', {
-    type: 'fade',
-    rewind: true,
-  });
-  splide8.mount();
+
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" activeSlide", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " activeSlide";
+}
